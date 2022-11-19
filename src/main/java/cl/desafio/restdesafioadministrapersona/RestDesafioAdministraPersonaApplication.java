9d4fa68381bf44ffbb19da13d56b2e531d1c57b6
Leitax <118635236+Leitax@users.noreply.github.com>
2022-11-19 20:54:26 -0300

    Primer Commit

---
+++ src/main/java/cl/desafio/restdesafioadministrapersona/RestDesafioAdministraPersonaApplication.java
@@ -0,0 +1,13 @@
+package cl.desafio.restdesafioadministrapersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDesafioAdministraPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDesafioAdministraPersonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/desafio/restdesafioadministrapersona/RestDesafioAdministraPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.desafio.restdesafioadministrapersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDesafioAdministraPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
